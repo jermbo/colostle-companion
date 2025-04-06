f3199291923bffc2a74dd475b353e961d442b0d3
jermbo <jermbo@users.noreply.github.com>
2025-04-06 11:53:45 -0400

    refactor: add themed variables for light and dark themes
    Introduced a new CSS file for themed variables to support light and dark themes. Updated index.css to import the new themed-variables.css, enhancing modularity and maintainability of styles.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./themed-variables.css";
-import "./styles/layout.css";
-import "./styles/ui.css";
 import "./index.css";
 import App from "./App.tsx";
 
