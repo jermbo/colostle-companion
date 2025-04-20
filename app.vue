fe777db337d72a82c5e65f0761e6c818898c3e06
jermbo <jermbo@users.noreply.github.com>
2025-04-20 10:43:14 -0400

    refactor: restructure project for Nuxt integration and remove unused files
    Converted project to use Nuxt framework, including the addition of nuxt.config.ts and related configurations. Removed obsolete files and directories, including React components and styles, to streamline the codebase. Updated .gitignore to reflect new build outputs and local environment files. Added initial app.vue template for basic structure.

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<h1>Hello World</h1>
+		<button>Click me</button>
+	</div>
+</template>
+
+<style>
+@use "pico";
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ["@nuxt/eslint", "@nuxt/icon",],
+	devtools: { enabled: true, },
+	compatibilityDate: "2024-11-01",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always",
+				indent: "tab",
+			},
+		},
+	},
+},);
